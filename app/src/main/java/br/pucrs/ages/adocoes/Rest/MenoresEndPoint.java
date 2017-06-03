84eec24a2a0e70150e560058cb873b499cb6fb78
Marcus Vinicius Kuquert <marcus.kuquert@icloud.com>
2017-06-03 11:49:51 -0300

    Add RestUtil + Menor.java + EndPoint

---
+++ app/src/main/java/br/pucrs/ages/adocoes/Rest/MenoresEndPoint.java
@@ -0,0 +1,26 @@
+package br.pucrs.ages.adocoes.Rest;
+
+import java.util.List;
+
+import br.pucrs.ages.adocoes.Model.Menor;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Created by kuquert on 03/06/17.
+ */
+
+public interface MenoresEndPoint {
+    @GET("/menores")
+    Call<List<Menor>> menores(@Header("access-token") String accessToken);
+
+    @GET("/menores/{menorId}")
+    Call<Menor> menor(@Path("menorId") String portfolioId, @Header("access-token") String accessToken);
+
+    @POST("/menores/{menorId}")
+    Call<Menor> updateMenor(@Path("menorId") String portfolioId, @Header("access-token") String accessToken, @Body Menor menor);
+}
