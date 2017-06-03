5d25299ebf5585d28ddbb7019d532d4a6576c90f
Carlos André Sousa Rodrigues <carlos.sousa@acad.pucrs.br>
2017-06-03 13:09:17 -0300

    Alterações do tutorial.

---
+++ app/src/main/java/br/pucrs/ages/adocoes/Fragments/CarlosFragment.java
@@ -0,0 +1,44 @@
+package br.pucrs.ages.adocoes.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+
+import br.pucrs.ages.adocoes.R;
+
+public class CarlosFragment extends Fragment {
+
+    private ProgressBar mProgressBar;
+
+    public CarlosFragment() { }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_carlos, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.portfolio_allocation_list);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.native_progress_bar);
+
+        mProgressBar.setVisibility(View.GONE);
+
+    }
+}
