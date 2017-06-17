5e1ee1572d33c1d0a07d01cb59049c603864bd66
Marcus Vinicius Kuquert <marcus.kuquert@icloud.com>
2017-06-17 14:58:21 -0300

    Add CadastroCompleto Activity + all related fragments

---
+++ app/src/main/java/br/pucrs/ages/adocoes/Fragments/CadastroCompleto/EstadoCivilFragment.java
@@ -0,0 +1,22 @@
+package br.pucrs.ages.adocoes.Fragments.CadastroCompleto;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.pucrs.ages.adocoes.R;
+
+/**
+ * Created by kuquert on 17/06/17.
+ */
+
+public class EstadoCivilFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_estado_civil, container, false);
+    }
+}
