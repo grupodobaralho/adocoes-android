4dfd1210f8259c6ba6d5d8bad76a66f8a26b27af
Matheus Vaccaro <matheusvaccaro@icloud.com>
2017-09-03 18:40:11 -0300

    Added MinorDetails java and xml files

---
+++ app/src/main/java/br/pucrs/ages/adocoes/MinorDetails/MinorDetails.java
@@ -0,0 +1,15 @@
+package br.pucrs.ages.adocoes.MinorDetails;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.pucrs.ages.adocoes.R;
+
+public class MinorDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minor_details);
+    }
+}
