3c7ca7d2f1b3ce442c0ab47d41e1b705f5870632
Matheus Vaccaro <matheusvaccaro@icloud.com>
2017-09-04 01:05:30 -0300

    Creating fragment to display common child information. Currently having problems with Bundle in fragment and a crash when trying to add the fragment to the activity,

--- app/src/main/java/br/pucrs/ages/adocoes/MenorDetails/MenorDetailsActivity.java
+++ app/src/main/java/br/pucrs/ages/adocoes/MenorDetails/MenorDetailsActivity.java
@@ -1,9 +1,15 @@
 package br.pucrs.ages.adocoes.MenorDetails;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 
+import br.pucrs.ages.adocoes.Model.Menor;
 import br.pucrs.ages.adocoes.R;
+
+/**
+ * Created by Matheus on 03/09/2017.
+ */
 
 public class MenorDetailsActivity extends AppCompatActivity {
 
@@ -12,9 +18,12 @@ public class MenorDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menor_details);
 
+        // TODO: Receber menor de um intent e passar para os fragments
+
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
+                    .add(R.id.common_information_layout, CommonMenorInformationFragment.newInstance(), "CommonInformation")
                     .add(R.id.menor_details_layout, ParentesListFragment.newInstance(), "ParentesList")
                     .commit();
         }
