1703db348c0a65c4070cbac638a22f682dd31aba
Juan Collin <juanlucas09@hotmail.com>
2017-09-08 20:02:28 -0300

    MenorDetailsButtonsFragment created

--- app/src/main/java/br/pucrs/ages/adocoes/MenorDetails/MenorDetailsActivity.java
+++ app/src/main/java/br/pucrs/ages/adocoes/MenorDetails/MenorDetailsActivity.java
@@ -26,6 +26,7 @@ public class MenorDetailsActivity extends AppCompatActivity {
                     .beginTransaction()
                     .add(R.id.menor_details_layout, CommonMenorInformationFragment.newInstance(menor), "CommonInformation")
                     .add(R.id.menor_details_layout, ParentesListFragment.newInstance(), "ParentesList")
+                    .add(R.id.menor_details_layout, MenorDetailsButtonsFragment.newInstance(), "Buttons")
                     .commit();
         }
     }
