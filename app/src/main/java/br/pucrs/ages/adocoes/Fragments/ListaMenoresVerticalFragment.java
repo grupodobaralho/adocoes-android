10dba851e95ea342abece7211d17bf8c4be36bcf
Matheus Vaccaro <matheusvaccaro@icloud.com>
2017-09-29 20:40:14 -0300

    Fixed rebase bugs

--- app/src/main/java/br/pucrs/ages/adocoes/Fragments/ListaMenoresVerticalFragment.java
+++ app/src/main/java/br/pucrs/ages/adocoes/Fragments/ListaMenoresVerticalFragment.java
@@ -1,10 +1,12 @@
 package br.pucrs.ages.adocoes.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -13,8 +15,9 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import br.pucrs.ages.adocoes.DatabaseHelper;
+import br.pucrs.ages.adocoes.MenorDetails.MenorDetailsActivity;
 import br.pucrs.ages.adocoes.Model.Menor;
 
 import static br.pucrs.ages.adocoes.R.id;
@@ -26,9 +29,11 @@ import static br.pucrs.ages.adocoes.R.layout;
 public class ListaMenoresVerticalFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
-    private ListaVerticalAdapter mListAdapter;
-    private ArrayList<Menor> items = new ArrayList<>();
+    private ListaVerticalAdapter mListaVerticalAdapter;
+    private ListaHorizontalAdapter mListaHorizontalAdapter;
+    private List<Menor> items = new ArrayList<>();
     protected MenuItem listagemHorizontal;
+    private PagerSnapHelper pagerSnapHelper = new PagerSnapHelper();
 
     public ListaMenoresVerticalFragment() {
         // Required empty public constructor
@@ -37,6 +42,9 @@ public class ListaMenoresVerticalFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        
     }
 
     @Override
@@ -53,45 +61,68 @@ public class ListaMenoresVerticalFragment extends Fragment {
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(layoutManager);
 
-        items.add(new Menor("Marcus Kuquert 1"));
-        items.add(new Menor("Marcus Kuquert 2"));
-        items.add(new Menor("Marcus Kuquert 3"));
-        items.add(new Menor("Marcus Kuquert 4"));
-        items.add(new Menor("Marcus Kuquert 5"));
-        items.add(new Menor("Marcus Kuquert 6"));
-        items.add(new Menor("Marcus Kuquert 7"));
-        items.add(new Menor("Marcus Kuquert 8"));
 
-        mListAdapter = new ListaVerticalAdapter(getActivity());
-        mListAdapter.setData(items);
+        mListaVerticalAdapter = new ListaVerticalAdapter(getActivity());
+        mListaHorizontalAdapter = new ListaHorizontalAdapter(getActivity());
 
-        mListAdapter.setOnMenorFavoritarListener(new ListaVerticalAdapter.OnMenorSelectedListener() {
+        mListaVerticalAdapter.setData(items);
+
+        mListaVerticalAdapter.setOnMenorFavoritarListener(new ListaVerticalAdapter.OnMenorSelectedListener() {
             @Override
             public void OnMenorItemSelected(Menor menor) {
                 // Coloque aqui a ação de favoritar :)
-                boolean isFavorite = DatabaseHelper.getInstance(getActivity()).contemMenor(menor);
-                if(isFavorite) {
-                    Toast.makeText(getActivity(), ";) Já favoritou " + menor.getNome(), Toast.LENGTH_SHORT).show();
-                    return;
-                }
 
-                boolean result = DatabaseHelper.getInstance(getActivity()).insereFavorito(menor);
-                if(result)
-                    Toast.makeText(getActivity(), "favoritou " + menor.getNome(), Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(getActivity(), "nao favoritou", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "favoritou " + menor.getNome(), Toast.LENGTH_SHORT).show();
             }
         });
 
-        mListAdapter.setListener(new ListaVerticalAdapter.OnMenorSelectedListener() {
+        mListaVerticalAdapter.setListener(new ListaVerticalAdapter.OnMenorSelectedListener() {
             @Override
             public void OnMenorItemSelected(Menor menor) {
                 // Coloque aqui a ação de ir para tela de detalhes :)
+                // Boa Roberto ;)
 
-                Toast.makeText(getActivity(), "selecionou " + menor.getNome(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), MenorDetailsActivity.class);
+                intent.putExtra(MenorDetailsActivity.EXTRA_MENOR, menor);
+                startActivity(intent);
             }
         });
 
-        mRecyclerView.setAdapter(mListAdapter);
+        mListaHorizontalAdapter.setOnMenorFavoritarListener(new ListaHorizontalAdapter.OnMenorSelectedListener() {
+            @Override
+            public void OnMenorItemSelected(Menor menor) {
+                Toast.makeText(getActivity(), "favoritou " + menor.getNome(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mListaHorizontalAdapter.setListener(new ListaHorizontalAdapter.OnMenorSelectedListener() {
+            @Override
+            public void OnMenorItemSelected(Menor menor) {
+                Intent intent = new Intent(getActivity(), MenorDetailsActivity.class);
+                intent.putExtra(MenorDetailsActivity.EXTRA_MENOR, menor);
+                startActivity(intent);
+            }
+        });
+
+        mRecyclerView.setAdapter(mListaVerticalAdapter);
     }
+
+    public void setItems(List<Menor> items, boolean isListagemVertical) {
+        this.items = items;
+        if (isListagemVertical){
+            pagerSnapHelper.attachToRecyclerView(null);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+            this.mRecyclerView.setLayoutManager(layoutManager);
+            this.mRecyclerView.setAdapter(mListaVerticalAdapter);
+            mListaVerticalAdapter.setData(items);
+        } else {
+            LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+            pagerSnapHelper.attachToRecyclerView(this.mRecyclerView);
+            this.mRecyclerView.setLayoutManager(layoutManager);
+            this.mRecyclerView.setAdapter(mListaHorizontalAdapter);
+            mListaHorizontalAdapter.setData(items);
+        }
+    }
+
+
 }
