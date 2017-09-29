cf2d4f871909ba54f050264c84dbcfd648e7434a
Matheus Vaccaro <matheusvaccaro@icloud.com>
2017-09-29 20:40:14 -0300

    Fixed rebase bugs

--- app/src/main/java/br/pucrs/ages/adocoes/Fragments/ListaMenoresCardFragment.java
+++ app/src/main/java/br/pucrs/ages/adocoes/Fragments/ListaMenoresCardFragment.java
@@ -42,7 +42,7 @@ public class ListaMenoresCardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista_menores_card, container, false);
+        return inflater.inflate(R.layout.fragment_lista_menores_horizontal, container, false);
     }
 
     @Override
