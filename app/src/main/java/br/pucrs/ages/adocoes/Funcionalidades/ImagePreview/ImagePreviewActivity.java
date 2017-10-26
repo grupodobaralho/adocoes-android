2a091c5743e2aa71c4677ef6e73a5ecc20ccd31f
Homero Oliveira <homero_junior_oliveira@outlook.com>
2017-10-26 18:43:51 -0200

    back button from ImagePreviewActivity

--- app/src/main/java/br/pucrs/ages/adocoes/Funcionalidades/ImagePreview/ImagePreviewActivity.java
+++ app/src/main/java/br/pucrs/ages/adocoes/Funcionalidades/ImagePreview/ImagePreviewActivity.java
@@ -2,7 +2,9 @@ package br.pucrs.ages.adocoes.Funcionalidades.ImagePreview;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.github.chrisbanes.photoview.PhotoView;
 
@@ -20,13 +22,29 @@ public class ImagePreviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_preview);
 
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         Intent intent = getIntent();
         if (intent != null) {
             final ArrayList<String> midiasIds = intent.getStringArrayListExtra(ViewPagerFragment.ARGUMENT_MIDIAS);
             final int position = intent.getIntExtra(EXTRA_POSITION, -1);
 
             final PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
-            photoView.setBi
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int itemId = item.getItemId();
+
+        if (itemId == android.R.id.home) {
+            this.onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
